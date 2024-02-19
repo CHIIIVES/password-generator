27e14fdd3f1783f4d0ea63dd226afa6d00ae1cae
Jordan <bloodyinsides@gmail.com>
2024-02-19 12:50:34 -0500

    Added basic structure of generatePassword function and began implementing random selection of characters

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,47 @@
 // Assignment code here
+// const uppers = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+// const uppers = Array.from({length: 26}, (_, i) => String.fromCharCode(65 + i));
+const uppers = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+const lowers = 'abcdefghijklmnopqrstuvwxyz'.split('');
+const numbers = '1234567890'.split('');
+const special = '!"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~'.split('');
+let criteria = [0, false, false, false, false];
+console.log(uppers);
+console.log(lowers);
+console.log(special);
 
+function getCriteria() {
+  const length = prompt("Enter password length:");
+  return [42, true, true, true, true];
+}
+
+function genUpper() {
+  return uppers[Math.floor(Math.random()*uppers.length)];
+}
+
+function genLower() {
+  return lowers[Math.floor(Math.random()*lowers.length)];
+}
+
+function genNumber() {
+  return numbers[Math.floor(Math.random()*numbers.length)];
+}
+
+function genSpecial() {
+  return special[Math.floor(Math.random()*special.length)];
+}
+
+function generatePassword() {
+  criteria = getCriteria();
+  let passString = "";
+  for (let i = 0; i < criteria[0]; i++)
+  {
+    passString += genUpper();
+    console.log("test");
+  }
+
+  return passString;
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
